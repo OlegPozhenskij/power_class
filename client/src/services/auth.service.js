b1be07f0e20dfe11ae82d88cd62311bf4ff779d2
Pozhenskii Oleg <pozhenskij2@gmail.com>
2021-01-25 17:08:03 +0400

    make it better

--- client/src/services/auth.service.js
+++ client/src/services/auth.service.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'https://secure-app-class.herokuapp.com/api/auth/';
+const API_URL = 'https://secure-class-app.herokuapp.com/api/auth/';
 
 class AuthService {
   login(user) {
--- client/src/services/user.service.js
+++ client/src/services/user.service.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import authHeader from './auth-header';
 
-const API_URL = 'https://secure-app-class.herokuapp.com/api/test/';
+const API_URL = 'https://secure-class-app.herokuapp.com/api/test/';
 
 class UserService {
   getPublicContent() {
